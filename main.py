adfa79c8b7335a849064dc9dc707be73806902e3
Marcel Suter <marcel.suter@bzz.ch>
2023-07-21 07:58:12 +0200

    added solution

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    # Write your program here and remove the line "pass"
-    pass
+    year = int(input('Give a year:\n'))
+    if year % 400 == 0:
+        print('The year is a leap year.')
+    elif (year % 4 == 0) and (year % 100 != 0):
+        print('The year is a leap year.')
+    else:
+        print('The year is not a leap year.')
 
 
 if __name__ == '__main__':
