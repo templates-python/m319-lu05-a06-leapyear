56a2073e4c035d85ab722debc38a5ab71cba216f
graphics80 <K.Ilkow@gmail.com>
2023-10-03 09:00:04 +0200

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -2,28 +2,28 @@ from main import main
 
 
 def test_oddyear(capsys, monkeypatch):
-    inputs = iter([2023])
+    inputs = iter(['2023'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
     assert captured.out == 'The year is not a leap year.\n'
 
 def test_four(capsys, monkeypatch):
-    inputs = iter([2024])
+    inputs = iter(['2024'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
     assert captured.out == 'The year is a leap year.\n'
 
 def test_hundered(capsys, monkeypatch):
-    inputs = iter([1900])
+    inputs = iter(['1900'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
     assert captured.out == 'The year is not a leap year.\n'
 
 def test_fourhundered(capsys, monkeypatch):
-    inputs = iter([2000])
+    inputs = iter(['2000'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
